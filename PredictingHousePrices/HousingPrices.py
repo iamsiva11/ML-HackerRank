6245df10a7e9136834fd2c6f90e40905c5952de4
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-01-29 13:57:22 +0530

    Cleaned code, added comments

--- PredictingHousePrices/HousingPrices.py
+++ PredictingHousePrices/HousingPrices.py
@@ -1,41 +1,38 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-
 class predictHousePrices:
 
 	def __init__(self):
 		self.xtrain = []
 		self.xtest  = []
+		self.ytrain=[]
 		self.T = None
 
 	def get_input(self):
+		#Features , No of samples - input 
 		F, N = [ int(i) for i in raw_input().strip().split() ]
-		
+		#Training data - input
 		for i in range(N):
-    			self.xtrain.append(map(float, raw_input().split() ))
-		
+    			self.xtrain.append(map(float, raw_input().split() ))			
+		#No of test cases for test data - input
 		self.T = int(raw_input())
-
+		#Test data - input
 		for i in range(self.T):
     			self.xtest.append(map(float, raw_input().split()) )
 
-
 	def format_data(self):
 		self.xtrain= np.array(self.xtrain)
-		self.ytrain= self.xtrain[:, -1]
-		self.xtrain= self.xtrain[:, :-1]
+		self.ytrain= self.xtrain[:, -1] #Last column
+		self.xtrain= self.xtrain[:, :-1] #Except the Last column
 		self.xtest = np.array(self.xtest)
-
 
 	def predict_prices(self):
 		lr = LinearRegression().fit(self.xtrain,self.ytrain)
 		preds = list(lr.predict(self.xtest))
 		return preds
 
-
 if __name__=="__main__":
-
 	phouse = predictHousePrices()
 	phouse.get_input()
 	phouse.format_data()
